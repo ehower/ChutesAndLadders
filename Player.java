6093bda57e640f819967f28902869cccdbcb3f1e
tuvn789 <55150555+tuvn789@users.noreply.github.com>
2020-01-28 11:29:44 -0500

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,1 +1,28 @@
 public class Player
+{
+	private int position;
+	private String name;
+	private int age;
+	private String race;
+
+	public Player(int p, String n, int a, String r)
+	{
+		position = p;
+		name = n;
+		age = a;
+		race = r;
+	}
+	public int getPosition()
+	{
+		return position;
+	}
+	public void setPosition(int p)
+	{
+		position = p;
+	}
+	public String toString()
+	{
+		String s = "Hi my name is " + name + ". My age is " + age + " and my race is " + race;
+		return s;
+	}
+}
