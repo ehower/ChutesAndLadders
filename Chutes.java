cf6bf2f197a3a6d904ccf8cb064e98399d1e9ead
tuvn789 <55150555+tuvn789@users.noreply.github.com>
2020-01-29 11:03:44 -0500

    Add files via upload

---
+++ Chutes.java
@@ -0,0 +1,18 @@
+public class Chutes
+{
+	private int start, end;
+
+	public Chutes(int s, int e)
+	{
+		start = s;
+		end = e;
+	}
+	public int getStart()
+	{
+		return start;
+	}
+	public int getEnd()
+	{
+		return end;
+	}
+}
---
+++ Ladders.java
@@ -0,0 +1,18 @@
+public class Ladders
+{
+	private int start, end;
+
+	public Ladders(int s, int e)
+	{
+		start = s;
+		end = e;
+	}
+	public int getStart()
+	{
+		return start;
+	}
+	public int getEnd()
+	{
+		return end;
+	}
+}
